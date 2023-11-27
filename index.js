94c979ec1ca53a2bb6717126dfc886c47f02f1b7
Asif359 <asifahammednishst@gmail.com>
2023-11-27 06:49:07 +0600

    implement pagination popular search and push comment and comment replay

--- index.js
+++ index.js
@@ -101,17 +101,184 @@ async function run() {
         });
         // get 
         app.get('/posts', async (req, res) => {
-            const result = await postCollection.find().sort({ postTime: -1 }).toArray();
-            res.send(result);
+            try {
+                const allPost = await postCollection.find().sort({ postTime: -1 }).toArray();
+
+                const page = parseInt(req.query.page) || 1;
+                const limit = parseInt(req.query.limit) || 10; // Set a default limit if not provided
+                //   const page =  1;
+                //   const limit =  5; 
+
+                const startIndex = (page - 1) * limit;
+                const endIndex = page * limit;
+
+                const results = {};
+                results.totalPost = allPost.length;
+                results.pageCount = Math.ceil(allPost.length / limit);
+
+                if (endIndex < allPost.length) {
+                    results.next = {
+                        page: page + 1,
+                    };
+                }
+
+                if (startIndex > 0) {
+                    results.prev = {
+                        page: page - 1,
+                    };
+                }
+
+                results.result = await postCollection.find().sort({ postTime: -1 }).skip(startIndex).limit(limit).toArray();
+                res.json(results);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Internal Server Error');
+            }
         });
         app.get('/posts/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await postCollection.findOne(query);
             res.send(result);
-          });
+        });
+        app.put('/posts/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatePost = req.body;
+            console.log(updatePost.authorImg);
+            const post = {
+                $set: {
+                    authorImg: updatePost.authorImg,
+                    authorName: updatePost.authorName,
+                    authorEmail: updatePost.authorEmail,
+                    postTitle: updatePost.postTitle,
+                    postDescription: updatePost.postDescription,
+                    postTag: updatePost.postTag,
+                    postComments: updatePost.postComments,
+                    upVote: updatePost.upVote,
+                    downVote: updatePost.downVote,
+                    postTime: updatePost.postTime,
+
+                }
+            }
+            const result = await postCollection.updateOne(filter, post, options);
+            res.send(result);
+        })
+        app.patch('/posts/:id/', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatePost = req.body;
+            // console.log(updatePost);
+            const post = {
+                $push: {
+                    postComments: updatePost.postComments,
+                }
+            }
+            const result = await postCollection.updateOne(filter, post, options);
+            res.send(result);
+        })
+
+        app.patch('/posts/:id/:commentId/:replayId', async (req, res) => {
+            const id = req.params.id;
+            const commentId = req.params.commentId;
+            const replayId = req.params.replayId;
+
+            // const filter = { _id: new ObjectId(postId), 'postComments.commentId': commentId };
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updatePost = req.body;
+
+            console.log("post Comment ,", updatePost.postComments);
+
+            let update;
+
+            if (replayId) {
+                update = {
+                    $push: {
+                        [`postComments.${commentId}.commentsReplay`]: updatePost.postComments.commentsReplay,
+                    },
+                };
+            }
+
+            const result = await postCollection.updateOne(filter, update, options);
+
+            res.send(result);
+        });
+
+        app.get('/popular', async (req, res) => {
+            try {
+                const allPost = await postCollection.find().sort({ postTime: -1 }).toArray();
+
+                const page = parseInt(req.query.page) || 1;
+                const limit = parseInt(req.query.limit) || 10; // Set a default limit if not provided
+                // const page =  1;
+                // const limit = 5;
+
+                const startIndex = (page - 1) * limit;
+                const endIndex = page * limit;
+
+                const results = {};
+                results.totalPost = allPost.length;
+                results.pageCount = Math.ceil(allPost.length / limit);
+
+                if (endIndex < allPost.length) {
+                    results.next = {
+                        page: page + 1,
+                    };
+                }
+
+                if (startIndex > 0) {
+                    results.prev = {
+                        page: page - 1,
+                    };
+                }
+
+                const popular = await postCollection.aggregate([
+                    {
+                        $addFields: {
+                            voteDifference: { $subtract: ['$upVote', '$downVote'] },
+                        },
+                    },
+                    {
+                        $sort: { voteDifference: -1 }, // Sort in descending order based on the voteDifference
+                    },
+                ]).toArray();
+
+                // Apply sorting, skip, and limit directly to the aggregated array
+                results.result = await popular.slice(startIndex, endIndex);
+
+                res.json(results);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Internal Server Error');
+            }
+        });
 
 
+        // app.get("/posts", async (req, res) => {
+
+        // })
+
+
+        // try {
+        //     const result = await postCollection.aggregate([
+        //         {
+        //             $addFields: {
+        //                 voteDifference: { $subtract: ['$upVote', '$downVote'] }
+        //             }
+        //         },
+        //         {
+        //             $sort: { voteDifference: -1 } // Sort in descending order based on the voteDifference
+        //         }
+        //     ]).toArray();
+
+        //     res.send(result);
+        // } catch (error) {
+        //     console.error('Error fetching and sorting posts:', error);
+        //     res.status(500).json({ error: 'Internal Server Error' });
+        // }
 
 
 
